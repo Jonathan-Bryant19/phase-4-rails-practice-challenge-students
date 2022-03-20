146609820d104a20e28fc502c689932e4d72d1d5
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-19 20:06:03 -0700

    Progress at 75 minutes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+
+    private
+
+	def not_found(not_found)
+        render json: { error: "#{not_found.model} not found" }, status: 404
+    end
+
+    def not_valid(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: 422
+    end
+    
 end
---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,33 @@
+class InstructorsController < ApplicationController
+  def index
+    instructors = Instructor.all
+    render json: instructors
+  end
+
+  def show
+    instructor = Instructor.find_by!(id: params[:id])
+    render json: instructor
+  end
+
+  def create
+    instructor = Instructor.create!(instructor_params)
+    render json: instructor, status: :created
+  end
+
+  def update
+    instructor = Instructor.find(params[:id])
+    instructor.update!(instructor_params)
+    render json: instructor, status: :ok
+  end
+
+  def destroy
+    instructor = Instructor.find_by!(id: params[:id])
+  end
+
+  private
+
+  def instructor_params
+    params.permit(:name)
+  end
+
+end
